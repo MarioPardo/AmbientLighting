81bef5c891269743e2a44beb5c4c74bdcffb2ef7
Mario Pardo <mario.pardo003@gmail.com>
2020-11-11 18:38:59 -0500

    Added Showcase Video

--- src/com/company/SerialOutput.java
+++ src/com/company/SerialOutput.java
@@ -23,7 +23,7 @@ public class SerialOutput
         sb = new StringBuilder();
         portsList = SerialPort.getCommPorts();
 
-        chosenPort = SerialPort.getCommPort("COM8");
+        chosenPort = SerialPort.getCommPort("COM4");
         chosenPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
         chosenPort.setBaudRate(1000000);
 
